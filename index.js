7b55ac554eaac631ed8f2bb771a435ed531a23fc
FalakNaz7862 <fnaz18095@gmail.com>
2025-08-05 22:49:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// Variable for String
+Q1: var username = "FalakNaz321";
+console.log(username);
+Q2: var myName = "FalakNaz";
+console.log(myName);
+Q3: var titleMessage;
+titleMessage = "Hello World";
+alert(titleMessage);
+Q4: var name = "Jhone Doe";
+alert(name);
+var age = "15 years old";
+alert(age);
+var certified = "Certified Mobile Application Development";
+alert(certified);
+Q5: alert("Pizza\nPizz\nPiz\nPi\nP");
+Q6: var email;
+emailaddress = "abc@gmail.com";
+alert(emailaddress);
+Q7: var book = "A Smarter way to learn JavaScript";
+book =
+  "I am trying to learn from the Book of A smarter way to learn JavaScript";
+alert(book);
+Q8:
+var line="Yah! I can write HTML content through JavaScript";
+document.writeln(line)
