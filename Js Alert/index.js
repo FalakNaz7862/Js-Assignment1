b2e92d68895dacf34ec3332f8b619b06f841ff84
FalakNaz7862 <fnaz18095@gmail.com>
2025-08-05 20:59:32 +0500

    Chapter-1

---
+++ Js Alert/index.js
@@ -0,0 +1,16 @@
+// Alert
+Q1:
+alert ("Assalam O Alikum")
+Q2:
+alert ("Error! Please enter a valid password")
+Q3:
+alert ("Welcome JS land..\n Happy Coding!")
+Q4:
+alert ("Welcome to JS land..")
+alert ("Happy Coding...\n Prevent this page from creating additional dialogs")
+Q5:
+alert ("Hello... I can run JS through my browser's console")
+
+
+
+
